c0abd1c120163e66e7a3e5aa72f158900d3a8ed3
OSUser <osuser@ubuntuos.localdomain>
2020-05-25 09:34:27 +0530

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
